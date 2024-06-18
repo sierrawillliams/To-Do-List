29d30be768b01a3e30044276645035cfc0ef49f5
Sierra Williams <78832175+sierrawillliams@users.noreply.github.com>
2024-06-17 19:06:30 -0700

    checkboxes added and styling fixed

--- ToDoList.js
+++ ToDoList.js
@@ -1,46 +1,55 @@
 function newElement() {
     var li = document.createElement("li");
+    
+    var checkbox = document.createElement("INPUT");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.className = "taskCheckbox";
+    
     var inputValue = document.getElementById("taskName").value;
     var t = document.createTextNode(inputValue);
-    li.appendChild(t);
+    
+    li.appendChild(checkbox); // Append the checkbox to the li element first
+    li.appendChild(t); // Then append the task name text
+
     if (inputValue === '') {
-      alert("You must write something!");
+        alert("You must enter a task!");
     } else {
-      document.getElementById("tasks").appendChild(li);
+        document.getElementById("tasks").appendChild(li);
     }
+    
     document.getElementById("taskName").value = "";
-  
+
+    var span = document.createElement("SPAN");
+    var txt1 = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt1);
+    li.appendChild(span);
+
+    var close = document.getElementsByClassName("close");
+    for (var i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+    var hr = document.createElement("hr");
+    li.appendChild(hr);
+}
+
+var myNodelist = document.getElementsByTagName("LI");
+for (var i = 0; i < myNodelist.length; i++) {
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
-    li.appendChild(span);
-  
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-      }
-    }
-  }
-
-
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+    myNodelist[i].appendChild(span);
 }
 
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
+for (var i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
 }
